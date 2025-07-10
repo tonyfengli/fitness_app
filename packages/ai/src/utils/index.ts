78a197f08fa3cb7f2a8bf74d42d41ca5a44633ac
Tony Li <tony.li.feng@gmail.com>
2025-07-10 16:27:13 -0700

    deleted more files

--- packages/ai/src/utils/index.ts
+++ packages/ai/src/utils/index.ts
@@ -1,8 +1,5 @@
 export * from "./fetchExercises";
-export * from "./selectRandomExercisesByMuscles";
 export * from "./formatWorkoutPlan";
-// Filtering functions moved to filtering module
-// export * from "./filterExercises";
 export * from "./businessValidation";
 export * from "./scoringCriteria";
 export * from "./templateOrganization";
